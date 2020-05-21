16296fc0686dc0bc8c4f7ea0f457b15d1e468151
JuniimSI <jrfernandesti@gmail.com>
2020-05-21 08:18:26 -0300

    Trabalhando de forma mais formal com o redux nos restaurants - criando as actions

--- src/components/list_restaurants/index.js
+++ src/components/list_restaurants/index.js
@@ -1,22 +1,24 @@
 import React, {Component} from 'react';
 import { Column } from "rbx";
-import store from "../../store";
-
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+ 
 import Restaurant from "./restaurant.js";
-
+import { loadRestaurants } from "../../actions/restaurant"; 
+ 
 class ListRestaurants extends Component {
   componentWillMount() {
-    this.restaurants = store.getState()["restaurantsState"];
+    this.props.loadRestaurants();
   }
-
+ 
   render() {
     return (
       <div className="restaurants-list">
         <h2 className="title is-size-4">Restaurantes</h2>
-
+ 
         <Column.Group multiline gapSize={2}>
           {
-            this.restaurants.map(restaurant => {
+            this.props.restaurants.map(restaurant => {
               return <Restaurant {...restaurant}/>
             })
           }
@@ -25,5 +27,11 @@ class ListRestaurants extends Component {
     )
   }
 }
-
-export default ListRestaurants;
+ 
+const mapStateToProps = store => ({
+  restaurants: store.restaurantsState.restaurants
+});
+ 
+const mapDispatchToProps = dispatch => bindActionCreators({ loadRestaurants }, dispatch);
+ 
+export default connect(mapStateToProps, mapDispatchToProps)(ListRestaurants);
